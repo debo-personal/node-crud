632d2e9f36598659d616b7d4db38c46e2a1f41e0
Debajit Majumder <debajit.business@gmail.com>
2016-09-25 13:48:57 +0530

    Successful connection created to mlab mongodB

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
 const app = express();
 
 app.set('view engine', 'ejs');
+var dB;
 
-app.listen( 3000, () => {
-	console.log('Node application started at port 3000');
+/*
+** Connection to mongo dB, on success of that we will start the node server.
+*/
+MongoClient.connect( 'mongodb://admin:password@ds033956.mlab.com:33956/debo-test-db', ( err, database) => {
+	if( err ) {
+		return console.error( "Couldn't connect to database. Error: ", err );
+	}
+	else {
+		app.listen( 3000, () => {
+			console.log('Node application started at port 3000');
+		});
+	}
 });
 
+/* Default route returns the index template */
 app.get('/', (req, res) => {
 	res.render( 'index.ejs' );
 });
