601c2d04c9cb23af521dcf63cbd3f2cc6d62c6b2
Debajit Majumder <debajit.business@gmail.com>
2016-09-25 10:29:12 +0530

    removed index.html, instead template engine used

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.listen( 3000, () => {
 	console.log('Node application started at port 3000');
 });
 
 app.get('/', (req, res) => {
-	res.sendFile( __dirname + '/index.html' );
+	res.render( 'index.ejs' );
 });
